72cb7886cca703ddbd7ec04618e5b956903a7d78
imadeddine ourak <79382663+imad06@users.noreply.github.com>
2025-02-17 06:12:35 +0100

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "https://chat-app-production-1876.up.railway.app" : "/api";;
+const BASE_URL = "https://chat-app-production-1876.up.railway.app" : "/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
