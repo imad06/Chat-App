86fb531cb358f7bb9c9013477c983e2f1d1ecde3
imadeddine ourak <79382663+imad06@users.noreply.github.com>
2025-02-17 05:59:37 +0100

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:5000/api" // URL locale de l'API pendant le développement
-    : "https://chat-app-production-1876.up.railway.app/api": "/api",, // URL de l'API en production sur Railway
+    : "https://chat-app-production-1876.up.railway.app/api": "/api", // URL de l'API en production sur Railway
   withCredentials: true,
 });
