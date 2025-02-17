7556b656001233d7c7cd4488b71a48c46c2aee5e
imadeddine ourak <79382663+imad06@users.noreply.github.com>
2025-02-17 04:06:56 +0100

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-production-1876.up.railway.app" : "/api",
   withCredentials: true,
 });
