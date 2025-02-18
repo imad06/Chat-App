0ea1b49575f553f7b5b5507089a7456a87ee6140
imadeddine ourak <79382663+imad06@users.noreply.github.com>
2025-02-18 01:32:53 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,6 +30,8 @@ app.use("/api/messages", messageRoutes);
 app.get('/api/status', (req, res) => {
     res.json({ message: "Backend is running!" });
 });
+app.use(express.json({ limit: '5mb' }));  
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 const io = new Server(server, {
   cors: {
