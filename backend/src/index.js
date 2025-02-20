addd3c009a510e4c8c042c2b7040168e794f6504
imadeddine ourak <79382663+imad06@users.noreply.github.com>
2025-02-20 01:25:06 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allows all origins
+    origin: "https://chatyrandom.netlify.app", // Allows all origins
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
