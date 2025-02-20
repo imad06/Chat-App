fdc9c53da2725dce3bddd54f92d0114cc5185747
imadeddine ourak <79382663+imad06@users.noreply.github.com>
2025-02-20 03:08:08 +0100

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import cors from "cors";
+
 const router = express.Router();
-router.use(cors());
+
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
